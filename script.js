da3941c829273e5c8f1fb95530817d3c09c8be56
Ramzbii <ramapratama1213@gmail.com>
2023-03-31 11:46:18 +0700

    membuat function update screen

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const numbers = document.querySelectorAll(".number")
 numbers.forEach((number)=>{
     number.addEventListener("click", (event) => {
-        console.log(event.target.value)
+        updateScreen(event.target.value)
     })
 })
+
+const calculatorScreen = document.querySelector('.calculator-screen')
+const updateScreen = (number)=>{
+    calculatorScreen.value=number
+}
